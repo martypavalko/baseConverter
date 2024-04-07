b934fad779731215920ba5f0146088c0ad66858e
Marty Pavalko <martypavalko@gmail.com>
2024-04-07 17:05:09 -0400

    initial commit, with hex to dec and dec to hex

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bconv
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"strings"
+	"unicode"
+)
+
+func hexToDec(hexValue string) int {
+    var sum int
+    for i, char := range hexValue {
+        var conversion float64 = 0
+        if float64(char - '0') > 9 {
+            switch unicode.ToUpper(char) {
+            case 'A':
+                conversion = 10
+            case 'B':
+                conversion = 11
+            case 'C':
+                conversion = 12
+            case 'D':
+                conversion = 13
+            case 'E':
+                conversion = 14
+            case 'F':
+                conversion = 15
+            }
+        } else {
+            conversion = float64(char - '0')
+        }
+
+        sum += int(conversion * math.Pow(16, float64(len(hexValue)-1-i)))
+    }
+
+    return sum
+
+}
+
+func hexValidate (hexValue string) bool {
+    for _, char := range hexValue {
+        var readyToValidate = int(unicode.ToUpper(rune(char)))
+        // log.Println(readyToValidate)
+        if 48 > readyToValidate || (57 < readyToValidate && 65 > readyToValidate) || 70 < readyToValidate {
+            fmt.Println("false")
+            return false
+        }
+    }
+   return true 
+}
+
+func decToHex (decValue float64) string {
+    var hexValue float64
+    var sb strings.Builder
+    log.Printf("decValue / 16 = %f\n", decValue/16)
+    for math.Floor(math.Mod(decValue, 16)) > 0 {
+        hexValue = math.Floor(math.Mod(decValue, 16))
+        log.Printf("Floored hexvalue = %f", hexValue)
+        switch hexValue {
+        case 10:
+            sb.WriteString("A")
+        case 11:
+            sb.WriteString("B")
+        case 12:
+            sb.WriteString("C")
+        case 13:
+            sb.WriteString("D")
+        case 14:
+            sb.WriteString("E")
+        case 15:
+            sb.WriteString("F")
+        default:
+            sb.WriteString(fmt.Sprintf("%.0f", hexValue))
+        }
+        decValue = decValue / 16
+    }
+    var hexOutput string
+    for _,v := range sb.String(){
+        hexOutput = string(v) + hexOutput
+    }
+    return hexOutput
+}
+
+func main() {
+    var hexValue string
+    var output int
+    fmt.Println("Please enter a hexadecimal value: ")
+    for {
+    fmt.Scanln(&hexValue)
+    output = hexToDec(hexValue)
+        if hexValidate(hexValue) {
+            fmt.Printf("%d\n", output)
+            break
+        } else {
+            fmt.Println("Input a valid hexadecimal value!  Try again!")
+        }
+    }
+    fmt.Println(decToHex(float64(output)))
+}
