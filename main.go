3e61624bc2f05b6381c43e4fe3176c146dcbddcf
Marty Pavalko <marty@martypavalko.com>
2024-06-15 15:03:30 -0400

    overhauled binary

--- main.go
+++ main.go
@@ -1,125 +1,68 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"os"
+
+	// "log"
 	"math"
-	"strings"
-	"unicode"
 )
 
-func hexToDec(hexValue string) int {
-    var sum int
-    for i, char := range hexValue {
-        var conversion float64 = 0
-        if float64(char - '0') > 9 {
-            switch unicode.ToUpper(char) {
-            case 'A':
-                conversion = 10
-            case 'B':
-                conversion = 11
-            case 'C':
-                conversion = 12
-            case 'D':
-                conversion = 13
-            case 'E':
-                conversion = 14
-            case 'F':
-                conversion = 15
+/*
+   Convert from:
+       binary <-> decimal
+       hexadecimal <-> decimal
+       binary <-> hexadecimal
+
+       **BONUS**
+       base64 encoding and decoding
+*/
+
+/*
+Steps for Binary conversion:
+  1. Core function which is conversion
+  2. Take input
+  3. Input validation
+*/
+
+func validateInput(userInput, conversionType string) (bool, error){
+    var invalid bool = false
+    var err error
+    if conversionType == "binary" {
+        for r := range(userInput) {
+            if r != '0' && r != '1' {
+                invalid = true
+                err = errors.New("Invalid binary string")
             }
-        } else {
-            conversion = float64(char - '0')
         }
-
-        sum += int(conversion * math.Pow(16, float64(len(hexValue)-1-i)))
     }
+    if invalid {
+        return false, err
+    }
+    return true, nil
+}
 
+// CURRENTLY RETURNING ERROR
+func binaryToDecimal(binaryValue string) float64 {
+    isValid, err := validateInput(binaryValue, "binary")
+    if err != nil || !isValid {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    size := len(binaryValue)
+    var sum float64
+    for exp, bit := range(binaryValue) {
+        if (bit != '0') {
+            sum += math.Pow(2, float64(size-exp-1))
+        }
+    }
     return sum
-
-}
-
-func hexValidate (hexValue string) bool {
-    for _, char := range hexValue {
-        var readyToValidate = int(unicode.ToUpper(rune(char)))
-        // log.Println(readyToValidate)
-        if 48 > readyToValidate || (57 < readyToValidate && 65 > readyToValidate) || 70 < readyToValidate {
-            fmt.Println("false")
-            return false
-        }
-    }
-   return true 
-}
-
-func decToHex (decValue int) string {
-    var floatDecValue = float64(decValue)
-    var hexValue float64
-    var sb strings.Builder
-    log.Printf("decValue / 16 = %f\n", floatDecValue/16)
-    for math.Floor(math.Mod(floatDecValue, 16)) > 0 {
-        hexValue = math.Floor(math.Mod(floatDecValue, 16))
-        log.Printf("Floored hexvalue = %f", hexValue)
-        switch hexValue {
-        case 10:
-            sb.WriteString("A")
-        case 11:
-            sb.WriteString("B")
-        case 12:
-            sb.WriteString("C")
-        case 13:
-            sb.WriteString("D")
-        case 14:
-            sb.WriteString("E")
-        case 15:
-            sb.WriteString("F")
-        default:
-            sb.WriteString(fmt.Sprintf("%.0f", hexValue))
-        }
-        decValue = decValue / 16
-    }
-    var hexOutput string
-    for _,v := range sb.String(){
-        hexOutput = string(v) + hexOutput
-    }
-    return hexOutput
-}
-
-func userPrompt() {
-    var answer string
-    fmt.Println("Please select a mode:")
-    fmt.Println("[1] Convert hexadecimal value to decimal")
-    fmt.Println("[2] Convert decimal value to hexadecimal")
-    for {
-        fmt.Scanln(&answer)
-        if answer == "1" {
-            var hexValue string
-            fmt.Println()
-            hexToDec(hexValue)
-            break
-        } else if answer == "2" {
-            var decValue string
-            decToHex(decValue)
-            break
-        } else {
-            log.Fatalln("Invalid Input! Try Again!")
-        }
-    }
-    
-    
 }
 
 func main() {
-    var hexValue string
-    var output int
-    fmt.Println("Please enter a hexadecimal value: ")
-    for {
-    fmt.Scanln(&hexValue)
-    output = hexToDec(hexValue)
-        if hexValidate(hexValue) {
-            fmt.Printf("%d\n", output)
-            break
-        } else {
-            fmt.Println("Input a valid hexadecimal value!  Try again!")
-        }
-    }
-    fmt.Println(decToHex(float64(output)))
+    fmt.Println(binaryToDecimal("010001"))
+    fmt.Println(binaryToDecimal("110000"))
+    fmt.Println(binaryToDecimal("11100001"))
+    fmt.Println(binaryToDecimal("000000011111"))
 }
