e7b5f8f91488822cdc0dad0a6398ef0ce5ca9930
Marty Pavalko <marty@martypavalko.com>
2024-06-18 19:08:14 -0400

    betterValidation func

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-
-	// "log"
 	"math"
 )
 
@@ -27,15 +25,22 @@ Steps for Binary conversion:
   3. Input validation
 */
 
-func validateInput(userInput, conversionType string) (bool, error){
+// TODO: Change return type from bool to float64 so that the output can be usable by whatever function receives it.
+func validateInput(userInput, conversionType string) (bool, error) {
     var invalid bool = false
     var err error
     if conversionType == "binary" {
         for _, r := range(userInput) {
             if r != '0' && r != '1' {
                 invalid = true
-                err = errors.New("Invalid binary string")
+                err = errors.New("Invalid binary value")
             }
+        }
+    } else if conversionType == "decimal" {
+        _, err := strconv.Atoi(userInput)
+        if err != nil {
+            invalid = true
+            err = errors.New("Invalid decimal value")
         }
     }
     if invalid {
@@ -43,6 +48,18 @@ func validateInput(userInput, conversionType string) (bool, error){
     }
     return true, nil
 }
+
+func betterValidation (userInput string) (string, error) {
+    // Binary return type: string
+    // Decimal return type: int/float
+    // Hexadecimal return type: string
+    // Base64 return type: string
+
+    // Take input, determine what base it is, validate the format is correct.
+    // If format is correct return, the original value and nil, if format is incorrect, return nil and the error 
+}
+
+// TODO: Move all parts of input validation to the validateInput function and have it automatically detect what type was entered.
 
 func binaryToDecimal(binaryValue string) string {
     isValid, err := validateInput(binaryValue, "binary")
@@ -60,9 +77,14 @@ func binaryToDecimal(binaryValue string) string {
     return strconv.Itoa(int(sum))
 }
 
-// func decimalToBinary(decimalValue string) string {
-//     return "Binary!"
-// }
+func decimalToBinary(decimalValue string) string {
+    isValid, err := validateInput(decimalValue, "decimal")
+    if err != nil || !isValid {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    return "Binary!"
+}
 
 func main() {
     fmt.Println(binaryToDecimal("010001"))
